bb6e8d221b73ccbfa39dcc957825df6a15319bd4
Frank3080 <frankortizcarballo@gmail.com>
2024-02-03 17:28:57 +0100

    9.1

---
+++ bmicalculator.ts
@@ -0,0 +1,9 @@
+const calculateBmi = (height: number, weight: number) => {
+  const meters: number = height / 100;
+  const bmi: number = weight / Math.pow(meters, 2);
+  if (bmi < 24.9 && bmi > 18.5) {
+    return "Normal (healthy weight)";
+  }
+};
+
+console.log(calculateBmi(180, 74));
